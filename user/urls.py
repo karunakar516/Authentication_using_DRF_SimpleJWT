eac05f10c1808f1200296634acc1cb7ff5de2a71
karunakar_516 <karunakarbalivada786.com>
2023-09-25 16:24:12 +0530

    router

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path,include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from .views import UserRegistrationView, UserLoginView,UserProfileUpdateView,UserDeleteView
-router=DefaultRouter()
-router.register('register/', UserRegistrationView.as_view(), basename='user-registration')
-router.register('profile/update/', UserProfileUpdateView.as_view(), name='profile-update')
-router.register('login/', UserLoginView.as_view(), name='user-login')
-router.register('profile/delete/',UserDeleteView.as_view(),name='user-delete')
+
 urlpatterns = [
-    path('',include(router.urls))
+    path('register/', UserRegistrationView.as_view(), name='user-registration'),
+    path('profile/update/', UserProfileUpdateView.as_view(), name='profile-update'),
+    path('login/', UserLoginView.as_view(), name='user-login'),
+    path('profile/delete/',UserDeleteView.as_view(),name='user-delete')
 ]
